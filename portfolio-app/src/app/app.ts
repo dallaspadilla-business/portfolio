4004bcf9832919606cb9ff8c1985a62bcad5b64f
dallaspadilla <42621411+dallaspadilla@users.noreply.github.com>
2025-09-23 20:20:33 -0700

    angular material added.

--- portfolio-app/src/app/app.ts
+++ portfolio-app/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule, MatIconModule, MatListModule],
+  imports: [RouterOutlet, MatCardModule, MatIconModule, MatListModule, MatExpansionModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
