2d87420493e3536156516d95906ab03090189330
dallaspadilla <42621411+dallaspadilla@users.noreply.github.com>
2025-09-23 17:16:21 -0700

    introducing more angular material to experience section

--- portfolio-app/src/app/app.ts
+++ portfolio-app/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule],
+  imports: [RouterOutlet, MatCardModule, MatIconModule, MatListModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio-app');
