8f9e66eb3a29e561929e35dc2765efed29e85dd5
dallaspadilla <42621411+dallaspadilla@users.noreply.github.com>
2025-09-25 20:48:52 -0700

    updated buttons on homepage

--- portfolio-app/src/app/home-page/home-page.ts
+++ portfolio-app/src/app/home-page/home-page.ts
@@ -4,11 +4,28 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink, MatButtonModule, MatCardModule, MatListModule, MatIconModule],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {}
+export class HomePage {
+  isDarkMode = false;
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    const themeClass = this.isDarkMode ? 'dark-theme' : 'light-theme';
+    document.body.classList.remove('dark-theme', 'light-theme');
+    document.body.classList.add(themeClass);
+  }
+}
