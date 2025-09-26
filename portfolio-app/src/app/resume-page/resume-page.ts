bdaf83becfa533de745001f827ce29b9cea7e5e8
dallaspadilla <42621411+dallaspadilla@users.noreply.github.com>
2025-09-25 20:52:19 -0700

    updated resume buttons

--- portfolio-app/src/app/resume-page/resume-page.ts
+++ portfolio-app/src/app/resume-page/resume-page.ts
@@ -3,10 +3,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-resume-page',
-  imports: [MatCardModule, MatIconModule, MatListModule, MatExpansionModule],
+  imports: [MatCardModule, MatIconModule, MatListModule, MatExpansionModule, MatButtonModule],
   templateUrl: './resume-page.html',
   styleUrl: './resume-page.scss',
 })
